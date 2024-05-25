8758d7915d1c42a12facca9635cbf5bffd2c504c
metachemist <meta.chemist.22@gmail.com>
2024-05-25 09:38:31 +0500

    Null Program

---
+++ null.js
@@ -0,0 +1,7 @@
+"use strict";
+let a = null;
+let sum = 0;
+if (a != null) {
+    sum = 10 + a;
+}
+console.log(sum);
---
+++ null.ts
@@ -0,0 +1,8 @@
+let a: null | number = null;
+let sum: number = 0;
+
+if (a!= null){
+    sum = 10 + a
+}
+
+console.log(sum);
